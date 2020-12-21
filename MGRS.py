f84dc458bfd730bcadc3e0a082f231168b1a5ac3
Markus Schaper <schaperm.80@gmail.com>
2020-12-21 20:53:32 +0100

    Formatierung

--- MGRS.py
+++ MGRS.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Version 1.0
+# Version 1.4
 # Markus Schaper
 # https://pypi.org/project/mgrs/
 # pip3 install mgrs
@@ -9,14 +9,12 @@ import mgrs
 latitude = 50.883954
 longitude = 7.090628
 
-print("Es werden zwei Variabeln definiert: latitude und longitude")
-print("Adresse: Bartholomäusstraße 30, 51145 Köln")
-
-print(latitude)
-print(longitude)
+print("Diese Adresse: Bartholomäusstraße 30, 51145 Köln wird in einen Längen- und Breitgrad umgewandelt.\n")
+print("Es werden zwei Variabeln definiert:", latitude, "und", longitude, "\n")
 
 m = mgrs.MGRS()
 c = m.toMGRS(latitude, longitude)
 
-print("Ausgabe UTM Rev oder MGRS Koordinate:")
-print(c)
+print("Ausgabe UTM Rev oder MGRS Koordinate:", c, "\n")
+
+print(c[0:3], c[3:5], c[5:10], c[10:15])
